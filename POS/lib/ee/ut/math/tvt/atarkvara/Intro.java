9d5f94c5d8a58dbb99092e736b8fddcf4a53e067
gamexh <hhaanneess@gmail.com>
2014-09-29 22:34:19 +0300

    Lisatud: lib kaust, Intro klass koos main'iga ja IntroUI.

---
+++ POS/lib/ee/ut/math/tvt/atarkvara/Intro.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.atarkvara;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello users");
+	}
+
+}
