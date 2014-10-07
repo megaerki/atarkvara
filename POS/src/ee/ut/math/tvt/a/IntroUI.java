eb2677e128f065f8a02d2527439921dea5a42b65
Erki Viidalepp <erki.viidalepp@gmail.com>
2014-10-07 14:37:02 +0300

    Parem app.prop lugemine

--- POS/src/ee/ut/math/tvt/a/IntroUI.java
+++ POS/src/ee/ut/math/tvt/a/IntroUI.java
@@ -4,84 +4,92 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+
 import javax.swing.JTextField;
+
 import java.awt.GridBagConstraints;
+
 import javax.swing.JLabel;
+
 import java.awt.Insets;
 import java.awt.Font;
 import java.awt.Canvas;
+
 import javax.swing.ImageIcon;
+
 import java.awt.Panel;
 import java.awt.Toolkit;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class IntroUI {
 
 	public JFrame frame;
 
-	public IntroUI() {
+	public IntroUI() throws IOException {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws IOException 
 	 */
-	private void initialize() {
+	private void initialize() throws IOException {
 		frame = new JFrame();
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(IntroUI.class.getResource("/logo.jpg")));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblProjektijuht = new JLabel("Projektijuht");
-		lblProjektijuht.setBounds(23, 24, 94, 14);
-		lblProjektijuht.setFont(new Font("Tahoma", Font.BOLD, 11));
-		frame.getContentPane().add(lblProjektijuht);
+		JLabel lblProjektiJuhtLabel = new JLabel("Projektijuht");
+		lblProjektiJuhtLabel.setBounds(23, 24, 94, 14);
+		lblProjektiJuhtLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		frame.getContentPane().add(lblProjektiJuhtLabel);
 		
-		JLabel lblErkiViidalepp = new JLabel("Erki Viidalepp");
-		lblErkiViidalepp.setBounds(23, 49, 82, 14);
-		frame.getContentPane().add(lblErkiViidalepp);
+		// Laeb propsid app.props failist.
+		Properties props = new Properties();
+		FileInputStream fis = new FileInputStream("application.properties");
+		props.load(fis);
 		
-		JLabel lblMegaerkiutee = new JLabel("megaerki@ut.ee");
-		lblMegaerkiutee.setBounds(177, 49, 99, 14);
-		frame.getContentPane().add(lblMegaerkiutee);
+	
 		
+		JLabel lblProjektiJuht = new JLabel(props.getProperty("projekt.juht")+", " + props.getProperty("projekt.juht.meil"));
+		lblProjektiJuht.setBounds(23, 49, 220, 16);
+		frame.getContentPane().add(lblProjektiJuht);
+	 		
 		JLabel lblLiikmed = new JLabel("Liikmed");
 		lblLiikmed.setBounds(23, 74, 82, 14);
 		lblLiikmed.setFont(new Font("Tahoma", Font.BOLD, 11));
 		frame.getContentPane().add(lblLiikmed);
 		
-		JLabel lblHannesHaljaste = new JLabel("Hannes Haljaste");
-		lblHannesHaljaste.setBounds(23, 122, 99, 14);
-		frame.getContentPane().add(lblHannesHaljaste);
+		JLabel lblProjektiLiige1 = new JLabel(props.getProperty("projekt.liige.1")+", "+props.getProperty("projekt.liige.1.meil"));
+		lblProjektiLiige1.setBounds(23, 122, 220, 16);
+		frame.getContentPane().add(lblProjektiLiige1);
 		
-		JLabel lblGamexhutee = new JLabel("gamexh@ut.ee");
-		lblGamexhutee.setBounds(177, 122, 97, 14);
-		frame.getContentPane().add(lblGamexhutee);
-		
-		JLabel lblKaarelViigi = new JLabel("Kaarel Viigi");
-		lblKaarelViigi.setBounds(23, 97, 87, 14);
-		frame.getContentPane().add(lblKaarelViigi);
+		JLabel lblProjektiLiige2 = new JLabel(props.getProperty("projekt.liige.2")+", "+props.getProperty("projekt.liige.2.meil"));
+		lblProjektiLiige2.setBounds(23, 97, 220, 16);
+		frame.getContentPane().add(lblProjektiLiige2);
 		
 		JLabel lblMeeskondA = new JLabel("Meeskond A");
 		lblMeeskondA.setBounds(90, 180, 119, 31);
 		lblMeeskondA.setFont(new Font("Vijaya", Font.PLAIN, 30));
 		frame.getContentPane().add(lblMeeskondA);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(IntroUI.class.getResource("/logo.jpg")));
-		lblNewLabel.setBounds(320, 140, 129, 96);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon(IntroUI.class.getResource("/logo.jpg")));
+		lblLogo.setBounds(320, 140, 129, 96);
+		frame.getContentPane().add(lblLogo);
 		
-		String versionNr = "@(#)0.1.12)@";
+		
+		String  versionNr = "@(#)0.1.14)@";
 		String versionSub = versionNr.substring(4, versionNr.length()-2);
 		JLabel lblNewLabelVersion = new JLabel("Versioon: " + versionSub);
 		lblNewLabelVersion.setBounds(320, 240, 200, 14);
 		frame.getContentPane().add(lblNewLabelVersion);
-		
-		JLabel lblKasnakantgmailcom = new JLabel("kasnakant@gmail.com");
-		lblKasnakantgmailcom.setBounds(177, 97, 171, 14);
-		frame.getContentPane().add(lblKasnakantgmailcom);
 	}
 }
