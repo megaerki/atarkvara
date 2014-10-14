afa624ab1d26e348986f1d1a1c96af511a907d7e
Gamexh <hhaanneess@gmail.com>
2014-10-14 13:21:13 +0300

    git push bat test

---
+++ POS/gitAnt.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+ECHO Git push launched
+
+REM Kommentaar: Üritame kasutada Windows cmd, et projekt üles pushida
+
+
+pause
--- POS/src/ee/ut/math/tvt/a/Intro.java
+++ POS/src/ee/ut/math/tvt/a/Intro.java
@@ -17,7 +17,6 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 	private static final String MODE = "console";
 	
-
 	public static void main(String[] args) throws IOException {
 		PropertyConfigurator.configure("application.properties");
 		
