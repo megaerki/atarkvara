87132bf3bc83fb1b21d147bbbe6c1c1bf30e7ad5
Kaarel <Kaarel@Kaarel-PC.lan>
2014-11-24 20:00:33 +0200

    väike muutatus buildis

--- POS/test/ee/ut/math/tvt/salessystem/ui/model/StockTableModelTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/ui/model/StockTableModelTest.java
@@ -1,10 +1,25 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
 import org.junit.Test;
+import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
 public class StockTableModelTest {
+	
+	private StockItem stockitem;
+	@Before
+	  public void setUp() {
+		  stockitem=new StockItem((long) 0, "Laua Viin", "viin", 5, 1);
+		
+	    
+	     
+	  }
 	
 	
 	
@@ -14,6 +29,9 @@ public class StockTableModelTest {
     }
 	@Test
     public void testHasEnoughInStock(){
+		
+		assertTrue(stockitem.getQuantity()>0);
+
     	
     }
 	@Test
