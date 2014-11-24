0245d05c4ddc8d4964a819eff8cdf8d9d5c3b9bb
Kaarel <Kaarel@Kaarel-PC.lan>
2014-11-24 17:27:31 +0200

    stockitemtest

--- POS/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -1,15 +1,34 @@
 package ee.ut.math.tvt.salessystem.domain.data;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class StockItemTest {
 	
+	private StockItem stockitem;
+	
+	  @Before
+	  public void setUp() {
+		  stockitem=new StockItem((long) 0, "Laua Viin", "viin", 5, 1);
+		
+	    
+	     
+	  }
+	
 	@Test
-    public void testClone(){
+    public void testClone() throws CloneNotSupportedException{//failib
+		clone();
+		
+		
+		
+	
     	
     }
 	@Test
     public void testGetColumn(){
+		assertEquals(stockitem.getColumn(1),"Laua Viin");
     	
     }
 
